0a07ff0ab6cd6869bc52e24eb2fbfb9bf8bf7b76
Aleksander <71639465+alpavlinov04@users.noreply.github.com>
2021-02-17 11:55:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TV TV1 = new TV();
+            TV TV2 = new TV("NEO", 64, 32);
+            Console.WriteLine(TV1);
+            Console.WriteLine(TV2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TV.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TV
+{
+    class TV
+    {
+        private string model;
+        private int size;
+        private int resolution;
+
+        public TV()
+        {
+            this.model = "Samsung";
+            this.size = 32;
+            this.resolution = 32;
+        }
+        public TV(string m, int s, int r)
+        {
+            this.model = m;
+            this.size = s;
+            this.resolution = r;
+        }
+        public void turnonTV(string person)
+        {
+            Console.WriteLine("The person " + person + "turn on the TV!");
+        }
+        public void turnoffTV(string person)
+        {
+            Console.WriteLine("The person " + person + "turn off the TV!");
+        }
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+        public int Size
+        {
+            get { return this.size; }
+            set { this.size = value;}
+        }
+        public int Resolution
+        {
+            get { return this.resolution; }
+            set { this.resolution = value; }
+        }
+        public override string ToString()
+        {
+            return "The model of the TV is " + this.model + " " + ", the size is " + this.size + " " + "and the resolution is " + this.resolution + "!";
+        }
+    }
+}
